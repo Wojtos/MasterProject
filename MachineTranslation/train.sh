3313c8e703bd72df890e99c2745ed6e20042148e
Wojtek Gruszka <wojtekgrucha@gmail.com>
2021-01-26 23:06:35 +0100

    changed used train mlm script

--- MachineTranslation/train.sh
+++ MachineTranslation/train.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 source venv/bin/activate
-python train_tokenizer.py
-python train_language_modeling.py
+python ./lib/transformers/examples/language-modeling/run_mlm.py \
+        --model_type bert \
+        --train_file ./raw_datasets/cycl.txt \
+        --validation_file ./raw_datasets/cycl-validation.txt \
+        --tokenizer_name ./cyclberto \
+        --line_by_line \
+        --do_train \
+        --do_eval \
+        --output_dir bert \
--- MachineTranslation/train_tokenizer.py
+++ MachineTranslation/train_tokenizer.py
@@ -1,12 +1,15 @@
 from tokenizers import ByteLevelBPETokenizer
 import os
+
+from transformers import RobertaConfig, RobertaTokenizerFast
+
 paths = ['./raw_datasets/cycl.txt']
 
 # Initialize a tokenizer
 tokenizer = ByteLevelBPETokenizer()
 
 # Customize training
-tokenizer.train(files=paths, vocab_size=52_000, min_frequency=2, special_tokens=[
+tokenizer.train(files=paths, vocab_size=52_000, special_tokens=[
     "<s>",
     "<pad>",
     "</s>",
@@ -18,3 +21,11 @@ model_path = './cyclberto'
 if not os.path.exists(model_path):
     os.mkdir(model_path)
 tokenizer.save_model(model_path)
+
+config = RobertaConfig(
+    vocab_size=52_000
+)
+
+tokenizer = RobertaTokenizerFast.from_pretrained(model_path, max_len=512)
+tokenizer.save_pretrained(model_path)
+config.save_pretrained(model_path)
