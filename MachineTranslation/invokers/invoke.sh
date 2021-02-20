d53cf6d9dcc529d23edebc31fbb9b95b5db1c897
Wojciech Gruszka <plgwgruszka@login02.prometheus.cyfronet.pl>
2021-02-20 14:48:00 +0100

    invokers change

--- MachineTranslation/invokers/invoke.sh
+++ MachineTranslation/invokers/invoke.sh
@@ -2,7 +2,7 @@
 #SBATCH -e ../logs/log-%j.err
 #SBATCH -o ../logs/log-%j.out
 #SBATCH --nodes 1
-#SBATCH --time=1:00:00
+#SBATCH --time=12:00:00
 #SBATCH --partition=plgrid-gpu-v100
 #SBATCH --gres=gpu:8
 #SBATCH --account=plglemkingpu3
--- MachineTranslation/invokers/train.sh
+++ MachineTranslation/invokers/train.sh
@@ -7,14 +7,12 @@ python ../lib/transformers/examples/seq2seq/run_seq2seq.py \
     --do_train \
     --do_eval \
     --task translation_en_to_cycl \
-    --dataset_name wmt16 \
-    --dataset_config_name ro-en \
     --source_lang en \
     --target_lang cycl \
     --train_file ../raw_datasets/train.json \
-    --validation_file ../validation.json \
+    --validation_file ../raw_datasets/validation.json \
     --output_dir ../models/en_cycl_$(date +%Y_%m_%d_%H_%M_%S) \
-    --per_device_train_batch_size=4 \
-    --per_device_eval_batch_size=4 \
+    --per_device_train_batch_size=64 \
+    --per_device_eval_batch_size=64 \
     --overwrite_output_dir \
     --predict_with_generate
