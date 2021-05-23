0247a9d5b0430357ef0cbac36f188747845283d2
Wojtek Gruszka <wojtekgrucha@gmail.com>
2021-05-23 17:57:45 +0200

    added t5 small training

--- MachineTranslation/language_modelling/train_tokenizer.py
+++ MachineTranslation/language_modelling/train_tokenizer.py
@@ -1,18 +1,23 @@
 from tokenizers import ByteLevelBPETokenizer
 import os
-
 from transformers import RobertaConfig, RobertaTokenizerFast
+import argparse
 
 paths = [
     '../raw_datasets/cycl-filtered.txt',
     '../raw_datasets/english-filtered.txt',
 ]
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', type=str)
+parser.add_argument('--vocab_size', type=int)
+args = parser.parse_args()
+
 # Initialize a tokenizer
 tokenizer = ByteLevelBPETokenizer()
 
 # Customize training
-tokenizer.train(files=paths, vocab_size=42024, special_tokens=[
+tokenizer.train(files=paths, vocab_size=args.vocab_size, special_tokens=[
     "<s>",
     "<pad>",
     "</s>",
@@ -20,13 +25,13 @@ tokenizer.train(files=paths, vocab_size=42024, special_tokens=[
     "<mask>",
 ])
 
-model_path = '../models/en_cycl_tokenizer'
+model_path = f'../models/{args.name}'
 if not os.path.exists(model_path):
     os.mkdir(model_path)
 tokenizer.save_model(model_path)
 
 config = RobertaConfig(
-    vocab_size=42024
+    vocab_size=args.vocab_size
 )
 
 tokenizer = RobertaTokenizerFast.from_pretrained(model_path, max_len=512)
