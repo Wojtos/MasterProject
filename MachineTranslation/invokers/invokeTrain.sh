11285dcbe4948b470f289ac97b51d6028edb79a1
Wojtek Gruszka <wojtekgrucha@gmail.com>
2021-06-19 13:48:00 +0200

    gpu usage optimization

---
+++ MachineTranslation/invokers/invokeTrain.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -e ../logs/log-%j.err
+#SBATCH -o ../logs/log-%j.out
+#SBATCH --nodes 1
+#SBATCH --time=12:00:00
+#SBATCH --partition=plgrid-gpu-v100
+#SBATCH --gres=gpu:8
+#SBATCH --account=plglemkingpu3
+
+module load plgrid/tools/python-intel/3.6.2
+module load plgrid/apps/cuda/10.1
+
+# Run given batch
+$1 do_train
